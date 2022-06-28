b125af3291cb418b3bb6cf4cdd39e97f147ca2d8
Mr.Siripoom Arthornsiriat <artorsiriratpoom@gmail.com>
2022-06-28 09:55:30 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+#import module
+from functools import update_wrapper
+import random
+import string
+from unittest import result
+
+print("Would you link to generate password?")
+#input length of password
+length = int(input("\n Enter your length password :"))
+#or define length
+length = 16
+#define data
+upper = string.ascii_uppercase
+lower = string.ascii_lowercase
+number = string.digits
+symbols = string.punctuation
+
+#combine
+all = upper + lower + number + symbols
+
+ran = random.sample(all,length)
+
+password = "".join(ran)
+
+print("Password",password)
